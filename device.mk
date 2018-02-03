35d2614004acca8efe88002bc5ef91f25f70a399
Abhiraj <abhirajsinghofficial@gmail.com>
2018-02-03 10:12:43 +0530

    ABC Changes

--- device.mk
+++ device.mk
@@ -21,6 +21,12 @@ $(call inherit-product, vendor/xiaomi/santoni/santoni-vendor.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
+# TARGET_BOARD_PLATFORM specific featurization
+QCOM_BOARD_PLATFORMS := msm8937
+
+# List of targets that use video hw
+MSM_VIDC_TARGET_LIST := msm8937
+
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
