33fe33aa83ad4ff6e1e08d25904742c5a6ad6fab
Nikesh001 <nikkataria74@gmail.com>
2018-02-09 10:47:25 +0000

    santoni: fix typo

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -192,7 +192,7 @@ TARGET_PROVIDES_LIBLIGHT := true
 
 # Lineage Hardware
 BOARD_HARDWARE_CLASS += \
-    $(DEVICE__PATH)/lineagehw
+    $(DEVICE_PATH)/lineagehw
 
 # Media
 TARGET_USES_MEDIA_EXTENSIONS := true
