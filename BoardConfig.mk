b70663c240734a384f9ff9db62fc6aa19342c11b
Abhiraj <abhirajsinghofficial@gmail.com>
2018-03-28 11:01:11 +0530

    Changes

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -48,7 +48,6 @@ BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE :=  2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 TARGET_KERNEL_CONFIG := santoni_defconfig
-KERNEL_TOOLCHAIN_PREFIX := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 TARGET_KERNEL_SOURCE := kernel/xiaomi/msm8937
 
 # ANT
